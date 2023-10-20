d7de609f0c0fbd435e1cce8e905aad1372ea7d63
Minleel <trminloc@gmail.com>
2023-10-20 13:38:31 +0700

    13:40 20/10

--- src/components/Schedule/CreateSlot.js
+++ src/components/Schedule/CreateSlot.js
@@ -1,19 +1,79 @@
-import React from "react";
+import { Stack } from 'react-bootstrap';
 
-const createData = {
-    date: "",
-    lecturer_id: "",
-    time: "",
-    duration: "",
-    room: "",
-}
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-const CreateSlot = () => {
+import { faEllipsisVertical } from '@fortawesome/free-solid-svg-icons'
+import { faXmark } from '@fortawesome/free-solid-svg-icons'
+import { faCircleInfo } from '@fortawesome/free-solid-svg-icons'
+import Style from '../../assets/style/form.module.scss'
+import { useContext } from 'react';
+import GlobalContext from '../../context/GlobalContext';
+import { useState } from 'react';
+import { getDateFormat } from '../../Utils/dateUtils'
+
+const subjects = [
+    'SWP301', 'SWR301', 'SWT301', 'PRF192', 'CEA201'
+]
+
+function CreateSlot() {
+
+    const [isHovered, setIsHovered] = useState(false);
+
+    const handleMouseEnter = () => {
+        setIsHovered(true);
+    };
+
+    const handleMouseLeave = () => {
+        setIsHovered(false);
+    };
+
+    const { showSlotModal, setShowSlotModal, selectedSlot, dispatchCalSlot } = useContext(GlobalContext)
+    console.log("BookPublic:" + showSlotModal)
+    const [purpose, setPurpose] = useState(
+        selectedSlot ? selectedSlot['slot'].purpose : ""
+    );
+
+    const slotData = selectedSlot['slot']
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const calendarSlot = {
+            // title,
+            // description,
+            // label: selectedLabel,
+            // day: daySelected.valueOf(),
+            // id: selectedEvent ? selectedEvent.id : Date.now(),
+            //  < p > Lecturer: { slotData.teacher }</p>
+            //                 <p>Slot: {slotData.slot}</p>
+            //                 <p>Date : {getDateFormat(slotData.date)}</p>
+            //                 <p>Time : {slotData.time}</p>
+            //                 <p>Room: {slotData.room}</p>
+            //                 <p>Status: {slotData.status}</p>
+            teacher: selectedSlot['slot'].teacher,
+            slot: selectedSlot['slot'].slot,
+            time: selectedSlot['slot'].time,
+            room: selectedSlot['slot'].room,
+            purpose: purpose,
+            status: 'waiting',
+        };
+        if (selectedEvent) {
+            dispatchCalSlot({ type: "update", payload: calendarSlot });
+        } else {
+            dispatchCalSlot({ type: "push", payload: calendarSlot });
+        }
+
+        setShowEventModal(false);
+    };
 
     return (
         <>
-            <form>
-            </form>
+            <div className={Style.box}>
+                <div className={Style.box_content}>
+
+                </div>
+            </div>
         </>
-    )
+    );
 }
+
+export default CreateSlot;
