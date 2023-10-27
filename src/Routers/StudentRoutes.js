efae304d684cad0c88fefd5e531b374e5e90360e
DUONG MINH HIEU <142511163+Mhiudangniengrang@users.noreply.github.com>
2023-10-27 11:47:41 +0700

    12:00 27/10 update FormSearch

--- src/Routers/StudentRoutes.js
+++ src/Routers/StudentRoutes.js
@@ -3,14 +3,12 @@ import S_Schedule from "../Pages/student/S_Schedule";
 import S_UserInfo from "../Pages/student/S_UserInfo";
 import S_ViewProfile from "../Pages/student/S_ViewProfile";
 import S_ViewTeacherProfile from "../Pages/student/S_ViewTeacherProfile";
-import S_SearchName from "../components/S_SearchTeacher/S_SearchName";
 
 const StudentRoutes = [
   { path: "/student", component: S_home },
   { path: "/student/viewinfo", component: S_UserInfo },
   { path: "/student/viewprofile", component: S_ViewProfile },
   { path: "/student/search/profileteacher", component: S_ViewTeacherProfile },
-  { path: "/student/searchteacher", component: S_SearchName },
   { path: "/student/viewschedule", component: S_Schedule },
 
 ];
